b4f87f50228181b033b6a2834a0d22b1a2947915
DRomanov <lukaviy2la@gmail.com>
2024-05-30 20:09:53 +0300

    new/ add front

--- frontend/src/components/CardCryptoInstance.jsx
+++ frontend/src/components/CardCryptoInstance.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
-import { Card } from 'antd';
+import {Card} from "antd";
+import numberWithCommas from "../utils.js";
 
 
-function CardCryptoInstance() {
+function CryptocurrencyCard(props) {
+  const { currency } = props
 
-    return (
-      <>
-       <Card
-      title={
-        <div className="flex items-center gap-5">
-            <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/2.png"/>
-            <span>LiteCoin</span>
-        </div>
-      }
-      extra={<a href="#">More</a>}
-      style={{
-        width: 300,
-      }}
-    >
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
-      </>
-    )
-  }
-  
-  export default CardCryptoInstance
+  const priceChangeColor = currency.quote.USD.percent_change_24h > 0 ? 'text-green-400' : 'text-red-400'
+  const formattedPrice = numberWithCommas(Math.round(currency.quote.USD.price))
+  const formattedMarketCap = numberWithCommas(Math.round(currency.quote.USD.market_cap/1_000_000_000))
+  const priceChange = Math.round(100 * currency.quote.USD.percent_change_24h) / 100
+
+  const price = Math.round(currency.quote.USD.price)
+
+  return (
+    <div>
+      <Card
+        title={
+          <div className="flex items-center gap-3">
+            <img src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${currency.id}.png`} alt='logo' width="50"/>
+            <p className="text-3xl">{currency.name}</p>
+          </div>
+        }
+        bordered={false}
+        style={{
+          width: 700,
+          height: 340,
+          'box-shadow': '0 3px 10px rgb(0,0,0,0.2)',
+        }}
+        className="text-2xl"
+      >
+        <p>Текущая цена: {formattedPrice}$</p>
+        <span>Изменение цены за 24 часа: </span>
+        <span className={priceChangeColor}>
+        {priceChange}%
+      </span>
+        <p>Текущая капитализация: ${formattedMarketCap}B</p>
+      </Card>
+    </div>
+  )
+}
+
+export default CryptocurrencyCard
   
--- service/main.py
+++ service/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 
 from service.endpoints import crypto_routers
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="Crypto Proxy!"
+)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(crypto_routers)
