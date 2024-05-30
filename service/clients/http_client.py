34bbd340b30b952dac6b5c633c1f55a9a645c6d6
DRomanov <lukaviy2la@gmail.com>
2024-05-30 20:15:12 +0300

    new/add cache

--- service/clients/http_client.py
+++ service/clients/http_client.py
@@ -1,14 +1,19 @@
+from async_lru import alru_cache
+
 from service.clients.http_base_client import HTTPClientBase
 
 
 class CMCClient(HTTPClientBase):
+
+    @alru_cache
     async def get_crypto_list(self):
         async with self._session.get(
             url="/v1/cryptocurrency/listings/latest"
         ) as response:
             result = await response.json()
             return result["data"]
-    
+        
+    @alru_cache
     async def get_crypto_instance(self, currency_id: int):
         async with self._session.get(
             url="/v2/cryptocurrency/quotes/latest",
